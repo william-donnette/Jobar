dbca0bd9349df3985012d074a0e9c6327f741e97
William Donnette <willdonn17@gmail.com>
2025-07-03 23:31:20 +0200

    feat: :sparkles: Introduce new Logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,48 +1,44 @@
 import {DefaultLogger} from '@temporalio/worker';
-import {createLogger, format, Logger, transports} from 'winston';
+import fs from 'fs';
+import path from 'path';
+import {createLogger, format, transports} from 'winston';
 
 export type LogLevel = 'DEBUG' | 'INFO' | 'WARN' | 'ERROR';
-/* istanbul ignore next */
+
 export const getDefaultLogger = (level: LogLevel) => {
+	const logDir = path.resolve('logs');
+	if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
+
+	const logger = createLogger({
+		level: level.toLowerCase(),
+		format: format.combine(
+			format.splat(),
+			format.timestamp(),
+			format.printf(({timestamp, level, message}) => `[${timestamp}] ${level}: ${message}`)
+		),
+		transports: [
+			new transports.Console({
+				format: format.combine(
+					format.colorize(),
+					format.splat(),
+					format.timestamp(),
+					format.printf(({timestamp, level, message}) => `[${timestamp}] ${level}: ${message}`)
+				),
+			}),
+			new transports.File({
+				filename: path.join(logDir, 'error.log'),
+				level: 'error',
+			}),
+			new transports.File({
+				filename: path.join(logDir, 'combined.log'),
+			}),
+		],
+	});
+
 	return new DefaultLogger(level, ({level, message}) => {
-		const lvl = level as LogLevel;
-		createLogger({
-			level: lvl,
-			transports: [
-				new transports.Console({
-					format: format.combine(
-						format.colorize(),
-						format.splat(),
-						format.timestamp(),
-						format.printf(({timestamp, level, message}) => {
-							return `[${timestamp}] ${level}: ${message}`;
-						})
-					),
-				}),
-				new transports.File({
-					dirname: 'logs',
-					filename: 'error.log',
-					level: 'error',
-					format: format.combine(
-						format.splat(),
-						format.timestamp(),
-						format.printf(({timestamp, level, message}) => {
-							return `[${timestamp}] ${level}: ${message}`;
-						})
-					),
-				}),
-				new transports.File({
-					dirname: 'logs',
-					filename: 'combined.log',
-					format: format.combine(
-						format.splat(),
-						format.timestamp(),
-						format.printf(({timestamp, level, message}) => {
-							return `[${timestamp}] ${level}: ${message}`;
-						})
-					),
-				}),
-			],
-		})[lvl.toLowerCase() as keyof Logger]?.(message);
+		logger.log({
+			level: level.toLowerCase(),
+			message,
+		});
 	});
 };
